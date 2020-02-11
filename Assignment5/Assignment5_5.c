02dda3098fcab2aacbdda93d110c2bffaa18a482
Ektaaa28 <chaudhariekta39@gmail.com>
2020-02-11 20:32:14 +0530

    Write a program which accept a number from user and return difference between summation of all its factors and non factors.

---
+++ Assignment5/Assignment5_5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int FactDiff(int iNo)
+{
+	int i,fact=0,nonfact=0,diff=0;
+	for(i=1;i<iNo;i++)
+	if(iNo%i==0)
+	fact=fact+i;
+	{
+		for(i=0;i<iNo;i++)
+		if(iNo%i!=0)
+		nonfact=nonfact+i;
+		return nonfact;
+		{
+			diff=fact-nonfact;
+			return diff;
+		}
+	}
+}
+int main()
+{
+	int iValue=0;
+	int iRet=0;
+	printf("Enter a number");
+	scanf("%d",&iValue);
+	iRet=FactDiff(iValue);
+	printf("%d",iRet);
+	return 0;
+
+}
