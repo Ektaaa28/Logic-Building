bfbd09d0525008914a224bd7feaa2ddbbc4307a3
Ektaaa28 <chaudhariekta39@gmail.com>
2020-03-14 21:27:18 +0530

    Write a program which accept range from user and display even numbers in between that range

---
+++ Assignment11/Assignment11_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void RangeDisplay(int iStart,int iEnd)
+{
+	int i;
+	for(i=iStart;i<=iEnd;i++)
+	{
+		if(i%2==0)
+		printf("%d\t",i);
+	}
+}
+int main()
+{
+	int iValue1=0,iValue2=0;
+	printf("Enter starting point");
+	scanf("%d",&iValue1);
+	printf("Enter ending point");
+	scanf("%d",&iValue2);
+	RangeDisplay(iValue1,iValue2);
+	return 0;
+}
+
