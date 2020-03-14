350dfa8299728139b25b682c4434f8f7d86de466
Ektaaa28 <chaudhariekta39@gmail.com>
2020-03-14 21:52:41 +0530

    Write a program which accept range from user and return addition of even numbers in between that range

---
+++ Assignment11/Assignment11_4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int RangeSum(int iStart,int iEnd)
+{
+	int i,sum=0;
+	for(i=iStart;i<=iEnd;i++)
+	if(i%2==0)
+	sum=sum+i;
+	return sum;
+}
+int main()
+{
+	int iValue1=0,iValue2=0,iRet=0;
+	printf("Enter starting point");
+	scanf("%d",&iValue1);
+	printf("Enter ending point");
+	scanf("%d",&iValue2);
+	iRet=RangeSum(iValue1,iValue2);
+	printf("Addition is %d",iRet);
+	return 0;
+}
